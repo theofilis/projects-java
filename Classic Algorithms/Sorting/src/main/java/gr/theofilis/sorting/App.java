fd3584bfc7965098553c1f55aa2b93c55a82fd7c
theofilis <theofilis.g@gmail.com>
2013-12-01 13:50:00 +0200

    Add classical Algorithms

---
+++ Classic Algorithms/Sorting/src/main/java/gr/theofilis/sorting/App.java
@@ -0,0 +1,13 @@
+package gr.theofilis.sorting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
